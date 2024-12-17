6ed852720482edd6dba39e1e5de0a53a9239ac23
AynazHamedi <aynaz.hamedi004@gmail.com>
2024-12-17 17:33:22 +0330

    mem

---
+++ xv6/sysproc.c
@@ -0,0 +1,128 @@
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+
+int
+sys_fork(void)
+{
+  return fork();
+}
+
+int
+sys_exit(void)
+{
+  exit();
+  return 0;  // not reached
+}
+
+int
+sys_wait(void)
+{
+  return wait();
+}
+
+int
+sys_kill(void)
+{
+  int pid;
+
+  if(argint(0, &pid) < 0)
+    return -1;
+  return kill(pid);
+}
+
+int
+sys_getpid(void)
+{
+  return myproc()->pid;
+}
+
+int
+sys_sbrk(void)
+{
+  int addr;
+  int n;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  addr = myproc()->sz;
+  if(growproc(n) < 0)
+    return -1;
+  return addr;
+}
+
+int
+sys_sleep(void)
+{
+  int n;
+  uint ticks0;
+
+  if(argint(0, &n) < 0)
+    return -1;
+  acquire(&tickslock);
+  ticks0 = ticks;
+  while(ticks - ticks0 < n){
+    if(myproc()->killed){
+      release(&tickslock);
+      return -1;
+    }
+    sleep(&ticks, &tickslock);
+  }
+  release(&tickslock);
+  return 0;
+}
+
+// return how many clock tick interrupts have occurred
+// since start.
+int
+sys_uptime(void)
+{
+  uint xticks;
+
+  acquire(&tickslock);
+  xticks = ticks;
+  release(&tickslock);
+  return xticks;
+}
+
+int
+sys_cpulim(void)
+{
+  int cpu_lim, mem_lim;
+  if(argint(0, &cpu_lim) < 0 || argint(1, &mem_lim) < 0){
+    return -1;
+  }
+
+  if(cpu_lim > 1000 || cpu_lim < 0){
+    return -1;
+  }
+
+  if(mem_lim < 0){
+    return -1;
+  }
+
+  cpu_time_limit = cpu_lim;
+  mem_limit = mem_lim;
+  return 0;
+}
+
+int 
+sys_memuse(void){
+  int amount;
+  if(argint(0, &amount) < 0){
+    return -1;
+  }
+
+  struct proc *p = myproc();
+  int new_amount = p->mem_used + amount;
+  if(new_amount < 0 || new_amount > mem_limit){
+    return -1;
+  }
+  p->mem_used += amount;
+  return 0;
+}
---
+++ xv6/umalloc.c
@@ -0,0 +1,106 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "param.h"
+
+// Memory allocator by Kernighan and Ritchie,
+// The C programming Language, 2nd ed.  Section 8.7.
+
+typedef long Align;
+
+union header {
+  struct {
+    union header *ptr;
+    uint size;
+  } s;
+  Align x;
+};
+
+typedef union header Header;
+
+static Header base;
+static Header *freep;
+
+void
+free(void *ap)
+{
+  Header *bp, *p;
+
+  Header *tempap = (Header*) ap;
+  int nbytes=((tempap->s.size-1)*sizeof(Header)+1)-sizeof(Header);
+  printf(1, "free: nbytes -> %d\n", nbytes);
+  memuse(-nbytes);
+
+  bp = (Header*)ap - 1;
+  for(p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+    if(p >= p->s.ptr && (bp > p || bp < p->s.ptr))
+      break;
+  if(bp + bp->s.size == p->s.ptr){
+    bp->s.size += p->s.ptr->s.size;
+    bp->s.ptr = p->s.ptr->s.ptr;
+  } else
+    bp->s.ptr = p->s.ptr;
+  if(p + p->s.size == bp){
+    p->s.size += bp->s.size;
+    p->s.ptr = bp->s.ptr;
+  } else
+    p->s.ptr = bp;
+  freep = p;
+}
+
+static Header*
+morecore(uint nu)
+{
+  char *p;
+  Header *hp;
+
+  if(nu < 4096)
+    nu = 4096;
+  p = sbrk(nu * sizeof(Header));
+  if(p == (char*)-1)
+    return 0;
+  hp = (Header*)p;
+  hp->s.size = nu;
+  free((void*)(hp + 1));
+  return freep;
+}
+
+void*
+malloc(uint nbytes)
+{
+  if(memuse(nbytes) < 0){
+    printf(1, "malloc: allocation not possible\n");
+    return 0;
+  }
+  printf(1, "malloc: allocation possible\n");
+
+  Header *p, *prevp;
+  uint nunits;
+
+  nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1;
+  if((prevp = freep) == 0){
+    base.s.ptr = freep = prevp = &base;
+    base.s.size = 0;
+  }
+  for(p = prevp->s.ptr; ; prevp = p, p = p->s.ptr){
+    if(p->s.size >= nunits){
+      if(p->s.size == nunits)
+        prevp->s.ptr = p->s.ptr;
+      else {
+        p->s.size -= nunits;
+        p += p->s.size;
+        p->s.size = nunits;
+      }
+      freep = prevp;
+      printf(1, "malloc: allocated memory %d\n", p->s.size);
+      return (void*)(p + 1);
+    }
+    if(p == freep)
+      if((p = morecore(nunits)) == 0)
+        // we have already allocated nbytes to the process so if at the end we could not allocate that much 
+        // of memory we need to update the mem_used of process
+        printf(1, "allocation failed");
+        memuse(-nbytes);
+        return 0;
+  }
+}
